42c0781de12f7eafe68ff51c29c4f197db3899b4
Jamesbarford <james.barford-evans@unibuddy.com>
2022-12-01 06:22:57 +0000

    day 2 pt2

--- day01/pt2/main.c
+++ day01/pt2/main.c
@@ -5,20 +5,48 @@
 #include "../../includes/readfile.h"
 
 #define toint(p) ((p)-48)
+#define ARRAY_SIZE (3)
+static unsigned int top_elves[ARRAY_SIZE] = {0};
+
+int idxOfMin(void) {
+    unsigned int min = UINT_MAX;
+    int idx = -1;
+    
+    for (int i = 0; i < ARRAY_SIZE; ++i) {
+        if (top_elves[i] < min) {
+            min = top_elves[i];
+            idx = i;
+        }
+    }
+
+    return idx;
+}
+
+void addElvesStash(unsigned int calories) {
+    int idx = idxOfMin();
+    if (idx != -1 && top_elves[idx] < calories) {
+        top_elves[idx] = calories;
+    }
+}
+
+unsigned int sumArray(void) {
+    unsigned int acc = 0;
+    for (int i = 0; i < ARRAY_SIZE; ++i) {
+        acc += top_elves[i];
+    }
+    return acc;
+}
 
 unsigned int solve(rFile *rf) {
     char *ptr = rf->buf;
-    unsigned int elf_with_most = 0u;
     unsigned int cur = 0u;
     unsigned int acc = 0u;
 
     while (*ptr) {
         if (*ptr == '\n' && (*(ptr + 1) == '\n' || *(ptr+1) == '\0')) {
             acc += cur;
+            addElvesStash(acc);
             cur ^= cur;
-            if (acc > elf_with_most) {
-                elf_with_most = acc;
-            }
             acc ^= acc;
         } else if (*ptr == '\n') {
             acc += cur;
@@ -29,12 +57,11 @@ unsigned int solve(rFile *rf) {
         ++ptr;
     }
 
-    return elf_with_most;
+    return sumArray();
 }
 
 int main(void) {
     rFile *rf = rFileRead("./input.txt");
-    solve(rf);
     printf("%u\n", solve(rf));
     rFileRelease(rf);
 }
